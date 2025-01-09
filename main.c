cb7c47128b97f06c9f8f2b537c93ecfa3b7e96fc
Alvaro297 <alvaromartinfalagan29@gmail.com>
2025-01-09 23:19:18 +0100

    Estructura realizada

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "philo.h"
+
+static void	philo(void)
+{
+
+}
+
+int	main(int argc, char **argv)
+{
+	if (ft_check_args(argc, argv))
+	{
+		printf("Error en los argumentos\n")
+		return (0);
+	}
+	philo();
+	return (0);
+}
---
+++ philo.h
@@ -0,0 +1,32 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+typedef struct s_args
+{
+	int		n_philos;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		number_eat;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print_lock;
+} t_args;
+
+typedef struct s_philo
+{
+	int			id;
+	pthread_t	thread;
+	int			n_times_eat;
+	long		last_meal_time;
+	int			is_fork;
+	t_args		*args;
+} t_philo;
+
+
+#endif
