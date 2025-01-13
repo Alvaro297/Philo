87199ecc4104d4e55304989e1361ed24b2fa3839
Alvaro Martin Falagan <alvamart@c2r17s3.42madrid.com>
2025-01-13 20:20:20 +0100

    Correcciones

--- utils.c
+++ utils.c
@@ -27,9 +27,9 @@ static void	ft_start_philo_help(t_args *args, char **argv, int argc)
 {
 	int		i;
 
-	i = 1;
-	args->forks = malloc(sizeof(pthread_mutex_t) * ft_atoi(argv[1]) + 1);
+	i = 0;
 	args->n_philos = ft_atoi(argv[1]);
+	args->forks = malloc(sizeof(pthread_mutex_t) * args->n_philos);
 	args->time_to_die = ft_atoi(argv[2]);
 	args->time_to_eat = ft_atoi(argv[3]);
 	args->time_to_sleep = ft_atoi(argv[4]);
@@ -37,7 +37,7 @@ static void	ft_start_philo_help(t_args *args, char **argv, int argc)
 		args->number_eat = ft_atoi(argv[5]);
 	else
 		args->number_eat = -1;
-	while (i <= argc)
+	while (i < args->n_philos)
 	{
 		if (pthread_mutex_init(&args->forks[i], NULL) != 0)
 			perror("Failed to initialize mutex");
@@ -50,25 +50,25 @@ static void	ft_start_philo_help(t_args *args, char **argv, int argc)
 t_philo		*ft_start_philo(char **argv, int argc)
 {
 	t_philo	*philo;
-	t_args	args;
+	t_args	*args;
 	int		i;
 
-	i = 1;
-	philo = malloc(sizeof(philo) * ft_atoi(argv[1] + 1));
-	ft_start_philo_help(&args, argv, argc);
-	while (i <= args.n_philos)
-	{
-		philo[i].id = i;
-		philo[i].args = &args;
+	i = 0;
+	args = malloc(sizeof(t_args));
+	ft_start_philo_help(args, argv, argc);
+	philo = malloc(sizeof(t_philo) * args->n_philos);
+	 while (i < args->n_philos) {
+		philo[i].id = i + 1;
 		philo[i].n_times_eat = 0;
 		gettimeofday(&philo[i].last_meal_time, NULL);
 		philo[i].right_fork = i;
-		if (args.n_philos == 1)
+		if (args->n_philos == 1)
 			philo[i].left_fork = -1;
-		else if (i == 1)
-			philo[i].left_fork = args.n_philos;
+		else if (i == 0)
+			philo[i].left_fork = args->n_philos - 1;
 		else
 			philo[i].left_fork = i - 1;
+		philo[i].args = args;
 		i++;
 	}
 	return (philo);
--- validations.c
+++ validations.c
@@ -19,13 +19,13 @@ int		ft_check_args(int argc, char **argv)
 	int		i;
 
 	i = 1;
-	if (argc != 5 && argc != 6)
+	if (argc < 5 || argc > 6)
 		return (1);
 	while (i < argc)
 	{
 		if (ft_atoi(argv[1]) == 0)
-			return (EXIT_FAILURE);
-		if (!isnumber(argv[i]))
+			return (1);
+		if (isnumber(argv[i]) == 1)
 			return (1);
 		i++;
 	}
